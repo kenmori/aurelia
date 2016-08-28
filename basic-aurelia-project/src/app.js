50b11fc8a11deda8d0b44d3651b457bf5eee24da
kenji <kennjimoritata@yahoo.co.jp>
2016-08-28 10:49:50 +0900

    add app.js

--- basic-aurelia-project/src/app.js
+++ basic-aurelia-project/src/app.js
@@ -4,3 +4,24 @@
 
 
 
+import {Todo} './todo';
+
+export class App {
+	constructor() {
+		this.heading = "Todos";
+		this.todos = [];
+		this.todoDescription = '';
+	}
+	addTodo(){
+		if(this.todoDescription) {
+			this.todos.push(new Todo(this.todoDescription));
+			this.todoDescription = '';
+		}
+	}
+	removeTodo(todo) {
+		let index = this.todos.indexOf(todo);
+		if(index !== -1){
+			this.todos.splice(index, i);
+		}
+	}
+}
